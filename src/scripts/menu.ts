8c46d4fee8ce937a4c0c1accff005f2f7a355c45
Leandro Ubilla <leandroubilla@protonmail.com>
2025-06-03 17:19:56 -0300

    Fix console log

--- src/scripts/menu.ts
+++ src/scripts/menu.ts
@@ -1,10 +1,8 @@
 document.addEventListener("astro:page-load", () => {
   document.querySelector('.hamburger')?.addEventListener('click', () => {
-    console.log('click')
     document.querySelector('#overlay')?.classList.toggle('hidden');
   });
   document.querySelector('#close-overlay')?.addEventListener('click', () => {
-    console.log('click')
     document.querySelector('#overlay')?.classList.toggle('hidden');
   });
 })
